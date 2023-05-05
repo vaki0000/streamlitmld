a717cdf08c101ed52d8c448de4eb41fba2742337
vaki <115734618+vaki0000@users.noreply.github.com>
2023-05-05 16:02:27 +0300

    Add files via upload

---
+++ my_app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from PIL import Image
+
+st.title("This is a title")
+st.text("This is some text")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
